1fcfcf65da5e1c34e66c04af2efb2e3b2dddce53
Hugo <hugocarbiener@gmail.com>
2021-11-17 12:41:07 +0100

    Add Main menu and level select

---
+++ tp-note/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public enum levels
+    {
+        level1,
+        level2,
+        level3,
+        level4,
+        level5,
+        level6,
+        level7,
+        level8,
+        level9,
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(levels.level1.ToString());
+    }
+    
+    public void Menu()
+    {
+        this.transform.Find("MainMenuUI").gameObject.SetActive(true);
+        this.transform.Find("LevelSelectUI").gameObject.SetActive(false);
+    }
+
+    public void SelectLevel()
+    {
+        this.transform.Find("MainMenuUI").gameObject.SetActive(false);
+        this.transform.Find("LevelSelectUI").gameObject.SetActive(true);
+    }
+
+    public void LoadLevel(levels level)
+    {
+        SceneManager.LoadScene(level.ToString());
+    }
+
+
+}
+
